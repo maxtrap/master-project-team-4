99a8e67b0db91b7ac2e90aaa5c92b308fa16a715
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-10-18 15:01:39 -0700

    add more libraries

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
+var scribble = new Scribble();
+
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(1600, 900);
 }
 
 function draw() {
-  background(220);
+    background(220);
+    scribble.scribbleEllipse(50, 50, 100, 100);
 }

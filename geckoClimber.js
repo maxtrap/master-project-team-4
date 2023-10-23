d6091b5d0800115f523522f249f054476255665f
Roberto Alan Zamudio <itsrob45@gmail.com>
2023-10-23 15:19:30 -0700

    Code From P5JS Editor Into VS

--- geckoClimber.js
+++ geckoClimber.js
@@ -1,5 +1,45 @@
 class GeckoClimber {
-  draw() {
-    ellipse(width / 2, height / 2, 40);
-  }
+    input;
+    toBeTyped = "Dog";
+    score = 0;
+    object;
+
+
+    constructor() {
+        this.input = createInput();
+        this.input.style('font-size', '20px');
+        this.input.position(windowWidth / 2.75, 700);
+        this.input.size(400);
+        textAlign(CENTER);
+        textSize(25);
+
+    }
+
+    draw() {
+        background("white");
+        text(this.toBeTyped, this.input.x + (this.input.width) / 2, 680);
+        fill("black");
+        this.object = ellipse(this.input.x + (this.input.width) / 2, windowHeight / 2, 50, 50)
+        this.typingSection();
+        text(this.score, this.input.x + (this.input.width) / 2, 20)
+    }
+
+    typingSection() {
+        if (keyCode == ENTER) {
+            if (this.input.value() == this.toBeTyped && keyCode == ENTER) {
+                keyCode = 0;
+                this.score += 10;
+                
+            }
+            if (this.input.value() != this.toBeTyped && keyCode == ENTER && keyIsDown(ENTER) == true) {
+                keyCode = 0;
+                this.score -= 5;
+            }
+            this.input.value('');
+
+
+        }
+    }
+
+
 }

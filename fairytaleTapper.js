83c2aaf963440e671b6c7c5686a125bef45e2337
Max Trapido <maxtrapido@gmail.com>
2023-10-26 14:13:16 -0700

    Refactor unicorn clickables in favor of Unicorn wrapper classes. Introduce unicorn.js

--- fairytaleTapper.js
+++ fairytaleTapper.js
@@ -9,18 +9,17 @@ let SPARKLES_FRAME_COUNT;
 
 function fairytalePreload() {
     UNICORN_IMG = loadImage('resources/unicorn.png');
-    SPARKLES_GIF = loadImage('resources/sparkles.gif', updateFrameCount);
+    SPARKLES_GIF = loadImage('resources/sparkles.gif', initializeFrameCount);
     BACKGROUND_IMG = loadImage('resources/castle-background.jpg');
 }
 
-function updateFrameCount() {
+function initializeFrameCount() {
     SPARKLES_FRAME_COUNT = SPARKLES_GIF.numFrames();
 }
 
 class FairytaleTapper {
     
     unicorns = [];
-    sparkles = [];
     score = 0;
     
 
@@ -31,18 +30,13 @@ class FairytaleTapper {
     draw () {
         background(BACKGROUND_IMG);
         this.unicorns.forEach(unicorn => {
-            unicorn.draw();
-            
-        });
-        
-        this.sparkles.forEach(sparkle => {
-            sparkle.draw(this);
+            unicorn.draw(this);
         });
         
         
         noStroke();
         fill(255, 200);
-        rect(0, 0, 300, 100);
+        rect(0, 0, 320, 100);
 
         textSize(64);
         fill(0);
@@ -51,25 +45,7 @@ class FairytaleTapper {
     }
 
     makeUnicorn() {
-        let unicorn = new Clickable();
-        unicorn.locate(getRandomX(), getRandomY());
-        unicorn.image = UNICORN_IMG;
-        unicorn.resize(UNICORN_WIDTH, UNICORN_HEIGHT);
-        unicorn.cornerRadius = 0;
-        unicorn.strokeWeight = 0;
-        unicorn.text = "";
-        unicorn.color = "nofill"
-        
-        unicorn.onPress = this.unicornPressed.bind(this, unicorn);
-
-        this.unicorns.push(unicorn);
-        return unicorn;
-    }
-
-    unicornPressed(uniPressed) {
-       this.unicorns = this.unicorns.filter(uni => uni !== uniPressed); 
-       this.sparkles.push(new Sparkles(uniPressed.x, uniPressed.y));
-       this.score++;
+        this.unicorns.push(new Unicorn(this));
     }
 
     generateUnicornAtRandomInterval() {
@@ -83,38 +59,3 @@ class FairytaleTapper {
     }
 }
 
-function getRandomX() {
-    return Math.floor(Math.random() * (width - UNICORN_WIDTH))
-}
-
-function getRandomY() {
-    return Math.floor(Math.random() * (height - UNICORN_HEIGHT))
-}
-
-
-class Sparkles {
-    currentFrame = 48;
-
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    draw(game) {
-        SPARKLES_GIF.setFrame(this.currentFrame)
-        this.currentFrame++;
-
-        // console.log(SPARKLES_GIF)
-        
-        image(SPARKLES_GIF, this.x, this.y, UNICORN_WIDTH, UNICORN_HEIGHT);
-        
-        if (this.currentFrame === SPARKLES_FRAME_COUNT) {
-            this.currentFrame = 0;
-        }
-
-        if (this.currentFrame === 47) {
-            game.sparkles = game.sparkles.filter(sparkle => sparkle !== this); 
-        }
-    }
-
-}

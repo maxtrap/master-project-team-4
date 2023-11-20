2434ffebed112cd87735048a67af570a7ebfd0fc
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-11-20 09:29:04 -0700

    implement way to have multiple paths at once

--- monkeyBonanza.js
+++ monkeyBonanza.js
@@ -19,23 +19,60 @@ class MonkeyBonanza {
   monkeyYTarget = 0;
   currentLevel = 0;
   pathways = [
-    function zigzagPath(x) {
-      return (
-        height / 2 -
-        50 -
-        100 * sin((x - (width / 2 - 150)) / 25) +
-        10 * sin((x - (width / 2 - 150)) / 12)
-      );
-    },
-    function straightPath(x) {
-      return height / 2 + 50;
-    },
-    function curvePath(x) {
-      return height / 2 + 100 + 30 * sin((x - (width / 2 - 150)) / 30);
-    },
+    [
+      function circleBottom(x) {
+        let radius = 100;
+        let centerX = width / 2; // Calculate the center x-coordinate
+        let centerY = height / 2;
+        let deltaX = x - centerX; // Find the horizontal distance from the center
+
+        // Check if the x-coordinate is within the circle's diameter
+        if (Math.abs(deltaX) > radius) {
+          return null; // The point is outside the circle
+        }
+
+        // Calculate the y-coordinate relative to the center of the screen
+        return centerY + Math.sqrt(radius * radius - deltaX * deltaX);
+      },
+      function circleTop(x) {
+        let radius = 100;
+        let centerX = width / 2; // Calculate the center x-coordinate
+        let centerY = height / 2;
+        let deltaX = x - centerX; // Find the horizontal distance from the center
+
+        // Check if the x-coordinate is within the circle's diameter
+        if (Math.abs(deltaX) > radius) {
+          return null; // The point is outside the circle
+        }
+
+        // Calculate the y-coordinate for the top half of the circle
+        return centerY - Math.sqrt(radius * radius - deltaX * deltaX);
+      },
+    ],
+    [
+      function zigzagPath(x) {
+        return (
+          height / 2 -
+          50 -
+          100 * sin((x - (width / 2 - 150)) / 25) +
+          10 * sin((x - (width / 2 - 150)) / 12)
+        );
+      },
+    ],
+    [
+      function straightPath(x) {
+        return height / 2 + 50;
+      },
+    ],
+    [
+      function curvePath(x) {
+        return height / 2 + 100 + 30 * sin((x - (width / 2 - 150)) / 30);
+      },
+    ],
   ];
   errorCount = 0;
   pathColor = "#FF0000";
+  currentPathwayGroup = () => this.pathways[this.currentLevel];
 
   constructor() {
     this.spawnBananas();
@@ -48,7 +85,11 @@ class MonkeyBonanza {
     this.monkeyYTarget = mouseY;
 
     this.checkIfTracing();
-    this.drawPathway(this.pathways[this.currentLevel]);
+
+    for (let i = 0; i < this.currentPathwayGroup().length; i++) {
+      this.drawPathway(this.currentPathwayGroup()[i]);
+    }
+
     this.drawMonkey();
     drawLevelIndicator(this.currentLevel);
   }
@@ -61,6 +102,7 @@ class MonkeyBonanza {
     beginShape();
     for (let i = 0; i < width; i++) {
       let y = pathFunction(i);
+      if (y === null) continue;
       vertex(i, y);
     }
     endShape();
@@ -70,6 +112,7 @@ class MonkeyBonanza {
     beginShape();
     for (let i = 0; i < width; i++) {
       let y = pathFunction(i);
+      if (y === null) continue;
       vertex(i, y);
     }
     endShape();
@@ -88,20 +131,21 @@ class MonkeyBonanza {
   }
 
   checkIfTracing() {
-    if (
-      this.isWithinThreshold(mouseX, mouseY, this.pathways[this.currentLevel])
-    ) {
-      this.pathColor = "#00FF00";
+    for (let i = 0; i < this.currentPathwayGroup().length; i++) {
+      if (
+        this.isWithinThreshold(mouseX, mouseY, this.currentPathwayGroup()[i])
+      ) {
+        this.pathColor = "#00FF00";
 
-      // Move the monkey along the pathway
-      this.monkeyXTarget = mouseX;
-      this.monkeyYTarget = this.pathways[this.currentLevel](mouseX);
-    } else {
-      this.pathColor = "#FF0000";
+        // Move the monkey along the pathway
+        this.monkeyXTarget = mouseX;
+        this.monkeyYTarget = this.currentPathwayGroup()[i](mouseX);
 
-      // Count an error if the monkey deviates from the pathway
-      this.errorCount++;
+        return;
+      }
     }
+    this.pathColor = "#FF0000";
+    this.errorCount++;
   }
 
   isWithinThreshold(x, y, pathFunction) {
